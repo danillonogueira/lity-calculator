116487d97e2b04e8a3526144fe33a593c3f41605
Danillo Nogueira <dnll_nog@yahoo.com>
2020-01-11 18:36:17 -0300

    minor naming factor

--- app/core/Calculator.js
+++ app/core/Calculator.js
@@ -9,16 +9,17 @@ export class Calculator {
    * @description Watches the click event on the calculator.
    */
   click() {
+    // Whether user clicked on a button.
     if (this._calculatorHelper.checkIfClickedOnBtn(event.target)) {
-      const character = event.target.getAttribute('data-meaning');
+      const meaning = event.target.getAttribute('data-meaning');
       
       // Whether user clicked on an button that triggers an operation.
       if (this._calculatorHelper.checkIfAction(event.target)) {
-        this._calculatorHelper.dispatch(character);
+        this._calculatorHelper.dispatch(meaning);
 
       // Whether user clicked on a button that inputs a number or symbol.
       } else {
-        this._calculatorHelper.print(character);
+        this._calculatorHelper.print(meaning);
       }
     }
   }

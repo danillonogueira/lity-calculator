a5bc847d5ecb2b37fb5e21f02c75bc6c5c3d8d45
Danillo Nogueira <dnll_nog@yahoo.com>
2020-12-22 16:13:21 -0300

    made electron work with everything

--- desktop.js
+++ desktop.js
@@ -1,6 +1,5 @@
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
+const { app, BrowserWindow } = require('electron');
+const createWindow = function() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -9,19 +8,18 @@ function createWindow () {
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('./dist/index.html')
 }
 
-app.whenReady().then(createWindow)
-
+app.whenReady()
+  .then(createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
-
+});
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+});
--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-import Calculator from './app/core/Calculator';
+import { Calculator } from './app/core/Calculator';
 import './app/styles/main.scss';
 
 const $ = document.querySelector.bind(document);
 const calculator = new Calculator($('#screen'));
 
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
 
 document.querySelectorAll.bind(document)('input[type="button"]').forEach((button) => {
   button.addEventListener('click', calculator.click.bind(calculator));
 });
+
